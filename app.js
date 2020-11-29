3b1d82982d98c462ea9676bad97882cd48f72538
YASHSVI GIRDHAR <45160510+YashsviG@users.noreply.github.com>
2020-11-28 19:20:28 -0800

    test tenor api

--- app.js
+++ app.js
@@ -3,15 +3,24 @@ const express = require('express');
 
 // App Setup
 const app = express();
-
+const bodyParser = require('body-parser')
 // Middleware
+const exphbs  = require('express-handlebars');
+const json = require('body-parser');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hello Squirrel');
-  });
-// Start Server
+    // set the url of the gif
+    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+    // render the hello-gif view, passing the gifUrl into the view to be displayed
+    res.render('hello-gif', { gifUrl })
+  })
 
+
+// Start Server
 app.listen(7000, () => {
   console.log('Gif Search listening on port localhost:7000!');
 });
