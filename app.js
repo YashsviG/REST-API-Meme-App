a01ae39f1fceee91bf24dd1e8c1ec00896d1a2c1
YASHSVI GIRDHAR <45160510+YashsviG@users.noreply.github.com>
2020-11-29 18:08:07 -0800

    Able to get giphy api url and other info the client side

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
 // Require Libraries
+const http = require('http');
+const path = require('path');
 const express = require('express');
-
-// App Setup
 const app = express();
-const bodyParser = require('body-parser')
+const giphyRandom = require("giphy-random");
+
 // Middleware
 const exphbs  = require('express-handlebars');
-const json = require('body-parser');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // Routes
+var giphyURL;
 app.get('/', (req, res) => {
-    // set the url of the gif
-    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-    // render the hello-gif view, passing the gifUrl into the view to be displayed
-    res.render('hello-gif', { gifUrl })
-  })
+    const url = "http://api.giphy.com/v1/gifs/search?api_key=qF9pp6xP32yEqyguSGNHYDKfUaAXJYQC&q=lol&limit=25&offset=0&rating=g&lang=en";
+    http.get(url, (response) => {
+        // SET ENCODING OF RESPONSE TO UTF8
+        response.setEncoding('utf8');
+        let body = '';
+        // listens for the event of the data buffer and stream
+        response.on('data', (d) => {
+            // CONTINUOUSLY UPDATE STREAM WITH DATA FROM GIPHY
+             body += d;
+        });
+        // once it gets data it parses it into json 
+        response.on('end', () => {
+            // WHEN DATA IS FULLY RECEIVED PARSE INTO JSON
+            let parsed = JSON.parse(body);
+            // RENDER THE HOME TEMPLATE AND PASS THE GIF DATA IN TO THE TEMPLATE
+            res.render('hello-gif', { gifs: parsed.data })
+        });
+    });
+    
+});
 
+// TO BE DONE
+app.delete('/delte/:id', function (req, res) {
+    const { id } = req.params;
+    res.send('Got a DELETE request at /user')
+  })
 
 // Start Server
 app.listen(7000, () => {
