4225204082b4aba4fd3dd87e18612a75819246eb
YASHSVI GIRDHAR <45160510+YashsviG@users.noreply.github.com>
2020-11-30 19:37:58 -0800

    Edit operation to be started

--- app.js
+++ app.js
@@ -3,20 +3,18 @@ const http = require('http');
 const path = require('path');
 const express = require('express');
 const app = express();
-const giphyRandom = require("giphy-random");
 
 // Middleware
 const exphbs  = require('express-handlebars');
-
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
 var url = "http://api.giphy.com/v1/gifs/search?api_key=qF9pp6xP32yEqyguSGNHYDKfUaAXJYQC&q=lol&limit=25&offset=0&rating=g&lang=en";
 var parsed;
+
 // Routes
-var giphyURL
+// GET METHOD
 app.get('/', (req, res) => {
-    //const url = "http://api.giphy.com/v1/gifs/search?api_key=qF9pp6xP32yEqyguSGNHYDKfUaAXJYQC&q=lol&limit=25&offset=0&rating=g&lang=en";
-    giphyURL = url;
     http.get(url, (response) => {
         // SET ENCODING OF RESPONSE TO UTF8
         response.setEncoding('utf8');
@@ -30,17 +28,16 @@ app.get('/', (req, res) => {
         response.on('end', () => {
             // WHEN DATA IS FULLY RECEIVED PARSE INTO JSON
             parsed = JSON.parse(body);
-            // RENDER THE HOME TEMPLATE AND PASS THE GIF DATA IN TO THE TEMPLATE
+            // RENDER THE TEMPLATE AND PASS THE GIF DATA IN TO THE TEMPLATE
             res.render('hello-gif', { gifs: parsed.data })
         });
     });
 });
 
-// TO BE DONE
+// DELETE OPERATION --> POST METHOD
 app.post('/delete/:id', function (req, res) {
     const ID  = req.params.id;
     var arrayID = [];
-    console.log("PARSED", parsed);
     
     for(var i = 0; i < parsed.data.length; i++) {
       if(parsed.data[i].id == ID) {
@@ -48,12 +45,25 @@ app.post('/delete/:id', function (req, res) {
         break;
       }
     }
-          
-        // RENDER THE HOME TEMPLATE AND PASS THE GIF DATA IN TO THE TEMPLATE
+        // RENDER THE TEMPLATE AND PASS THE GIF DATA IN TO THE TEMPLATE
          res.render('hello-gif', { gifs: parsed.data })
-
 })
 
+// TO BE DONE
+app.post('/edit/:id', function (req, res) {
+  const ID  = req.params.id;
+  var arrayID = [];
+  
+  
+  for(var i = 0; i < parsed.data.length; i++) {
+    if(parsed.data[i].id == ID) {
+      var x = parsed.data.splice(i, 1);
+      break;
+    }
+  }
+      // RENDER THE TEMPLATE AND PASS THE GIF DATA IN TO THE TEMPLATE
+       res.render('hello-gif', { gifs: parsed.data })
+})
 // Start Server
 app.listen(7000, () => {
   console.log('Gif Search listening on port localhost:7000!');
